5263a86c71bdd3b213fb5d6e456861109c9f5701
gysobu <gysobu@gmail.com>
2019-05-23 18:29:11 -0500

    added products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Switch,Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -12,11 +13,13 @@ class App extends Component {
   render() {
     return (
      <React.Fragment>
-       <Navbar></Navbar>
-       <ProductList/>
-       <Details/>
-       <Cart/>
-       <Default/>
+       <Navbar/>
+       <Switch>
+         <Route exact path="/" component={ProductList} />
+         <Route path="/details" component={Details} />
+         <Route path="/cart" component={Cart} />
+         <Route  component={Default} />
+       </Switch>
      </React.Fragment>
     );
   }
